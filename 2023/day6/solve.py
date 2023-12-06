16578116fd4d47c6a363587de8348d0f7a6ffff3
kstodolak <kapi199726@gmail.com>
2023-12-06 07:03:11 +0100

    2023 day 6

---
+++ 2023/day6/solve.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import os,re
+
+def calculate_distance(hold,time):
+  time_left = time-hold
+  speed=hold
+  dist=0
+  for t in range(time_left):
+    dist += speed
+  return dist
+
+
+with open(os.path.dirname(__file__) + '/input.txt') as f:
+  lines = f.read().splitlines()
+  times = list(map(int,re.findall(r'\d+', lines[0])))
+  distances = list(map(int,re.findall(r'\d+', lines[1])))
+
+  record_beaten = [0 for x in times]
+  for i,time in enumerate(times):
+    max_distance=0
+    for t in range(time):
+      dist = calculate_distance(t,time)
+      if dist >= max_distance: 
+        max_distance = dist
+      if dist > distances[i]: 
+        record_beaten[i] += 1
+
+
+  part1=1
+  for x in record_beaten:
+    part1 *= x
+  print(part1)
---
+++ 2023/day6/solve2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import os,re
+
+def calculate_distance(hold,time):
+  time_left = time-hold
+  speed=hold
+  dist=0
+  for t in range(time_left):
+    dist += speed
+  return dist
+
+def find_break(time, record):
+  lo = 0
+  hi = time // 2
+
+  while lo+1 < hi:
+    m = (lo+hi) // 2
+    d = calculate_distance(m, time)
+    if d >= record:
+      hi = m
+    else: 
+      lo = m
+  
+  return hi
+
+with open(os.path.dirname(__file__) + '/input.txt') as f:
+  lines = list(map(lambda x: x.replace(' ', ''), f.read().splitlines()))
+  time = list(map(int,re.findall(r'\d+', lines[0])))[0]
+  distance = list(map(int,re.findall(r'\d+', lines[1])))[0]
+
+  record_beaten = 0
+  time_to_record = find_break(time, distance)
+  
+  print(time-(2*time_to_record)+1)
