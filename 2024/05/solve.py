02da18b5efa004ab2fd11b76670b426a84639b5a
Kacper Stodolak <kapi199726@gmail.com>
2024-12-08 17:53:44 +0100

    2024 - Day 5 but slightly better

--- 2024/05/solve.py
+++ 2024/05/solve.py
@@ -9,36 +9,13 @@ with open(os.path.join(os.path.dirname(__file__), 'input')) as f:
       a, b = (int(x) for x in rule.split('|'))
       if a not in orders:
         orders[a] = set()
-
       orders[a].add(b)
 
-    good_order = []
-    bad_idx = []
-    for idx_update, _update in enumerate(updates):
+    part1 = 0
+    part2 = 0
+    for _update in updates:
       update = [int(x) for x in _update.split(',')]
       OK = True
-      for idx,i in enumerate(update[1:], 1):
-        if i in orders:
-          i_should_be_before = orders[i]
-          for j in update[:idx]:
-            if j in i_should_be_before:
-              bad_idx.append(idx_update)
-              OK = False
-              break
-          if not OK:
-            break
-      if OK:
-        good_order.append(update)
-
-    part1 = 0
-    for good in good_order:
-      part1 += good[len(good)//2]
-
-    print(part1)
-
-    part2 = 0
-    for b in bad_idx:
-      update = [int(x) for x in updates[b].split(',')]
       corrected = [update[0]]
       for idx, i in enumerate(update[1:], 1):
         if i not in orders:
@@ -51,11 +28,15 @@ with open(os.path.join(os.path.dirname(__file__), 'input')) as f:
               already_in_idx.append(corrected.index(ii))
           if len(already_in_idx) > 0:
             corrected.insert(min(already_in_idx), i)
+            OK = False
           else:
             corrected.append(i)
+      if OK:
+        part1 += corrected[len(corrected)//2]
+      else:
+        part2 += corrected[len(corrected)//2]
 
-      part2 += corrected[len(corrected)//2]
-
+    print(part1)
     print(part2)
 
 
